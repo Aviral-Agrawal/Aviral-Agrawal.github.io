4ef79e582b6f846424586ee8cfd972823f6ed774
Aviral Agrawal <aviralagrawal@rocketmail.com>
2023-08-28 18:53:21 -0400

    Update social

--- src/data/Social.js
+++ src/data/Social.js
@@ -2,18 +2,12 @@ import github from '../assets/images/github.svg'
 import linkedin from '../assets/images/linkedin.svg'
 
 export default [
+
     {
-        id: 3,
-        title: "Github",
-        icon: github,
-        linkTo: true,
-        path: "https://github.com/Aviral-Agrawal"
-    },
-    {
-        id: 4,
+        id: 1,
         title: "LinkedIn",
         icon: linkedin,
         linkTo: true,
         path: "https://linkedin.com/in/aviral-agrawal-783a01162 "
-    },
+    }
 ];
