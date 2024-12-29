26e16761a452e6fe788a0d8ec1351dd14eace9cb
Aviral Agrawal <aviralagrawal@rocketmail.com>
2024-12-29 22:41:00 +0530

    Add google scholar link

--- src/data/Social.js
+++ src/data/Social.js
@@ -1,5 +1,6 @@
 import github from '../assets/images/github.svg'
 import linkedin from '../assets/images/linkedin.svg'
+import scholar from '../assets/images/google_scholar_logo.svg'
 
 export default [
 
@@ -9,5 +10,12 @@ export default [
         icon: linkedin,
         linkTo: true,
         path: "https://linkedin.com/in/aviral-agrawal-783a01162 "
-    }
+    },
+    {
+        id: 2,
+        title: "Google Scholar",
+        icon: scholar,
+        linkTo: true,
+        path: "https://scholar.google.com/citations?user=NiW49k4AAAAJ&hl=en"
+    },
 ];
